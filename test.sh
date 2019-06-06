565266ea3e6f427e903a6e5766266d05700492b7
Saswati Ray <sray@cs.cmu.edu>
2019-06-06 14:56:18 -0400

    Some minor updates to evaluation script.

--- test.sh
+++ test.sh
@@ -1,29 +1,30 @@
 #!/usr/bin/env bash
 
-export D3MOUTPUTDIR=/home/sray/cmu-ta2/output
+export D3MOUTPUTDIR=/home/sray/cmu-ta2/cmu-ta2/output
 export D3MSTATICDIR=/home/sray
 export D3MDATADIR=/home/sray/DARPA_D3M
 
 export D3MTIMEOUT=1200
 export D3MCPU=8
 
-datasets=(185_baseball)
-targets=(Hall_of_Fame)
-metrics=(F1Macro)
+datasets=(196_autoMpg)
+targets=(class)
+metrics=(MSE)
+
+rm scores.csv
 
 for (( i=0; i<1; i++ ))
 do
     rm ./output/pipelines_ranked/*
     rm ./output/pipelines_searched/*
 
-	export D3MINPUTDIR="$D3MDATADIR/${datasets[i]}"
+    export D3MINPUTDIR="$D3MDATADIR/${datasets[i]}"
     export D3MPROBLEMPATH="$D3MDATADIR/${datasets[i]}/TRAIN/problem_TRAIN/problemDoc.json"
 
-	./src/main.py search
+    ./src/main.py search
 
     for jsonfile in ./output/pipelines_ranked/*
     do
-
         echo ${jsonfile}
         grep "pipeline_rank" ${jsonfile} >> scores.csv
         grep "classification" ${jsonfile} | awk -F':' '{print $2}' >> scores.csv
@@ -34,3 +35,5 @@ do
 
 	done
 done
+
+cat scores.csv
