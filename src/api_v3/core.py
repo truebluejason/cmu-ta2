88a2cfad1a2b326abd359c3ac90ca912ab0420b7
Saswati Ray <sray@cs.cmu.edu>
2019-07-18 16:52:34 -0400

    Add try-except around writing JSON pipeline.

--- src/api_v3/core.py
+++ src/api_v3/core.py
@@ -322,7 +322,11 @@ class Core(core_pb2_grpc.CoreServicer):
             #    logging.info(sys.exc_info()[0])
             score = self._solutions[solution_id].rank
             outputDir = os.environ['D3MOUTPUTDIR']
-            util.write_pipeline_json(self._solutions[solution_id], self._primitives, outputDir + "/pipelines_scored")
+            try:
+                util.write_pipeline_json(self._solutions[solution_id], self._primitives, outputDir + "/pipelines_scored")
+            except:
+                logging.info(sys.exc_info()[0])
+                logging.info(self._solutions[solution_id].primitives)
             logging.info("Score = %f", score)
             send_scores.append(core_pb2.Score(metric=request_params.performance_metrics[0],
              fold=0, value=value_pb2.Value(raw=value_pb2.ValueRaw(double=score)), random_seed=0))
